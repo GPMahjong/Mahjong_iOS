f923e90d568256b0df4a7c8f19d333b78c9c7dc3
Tao, Wang <Wang.Tao@activenetwork.com>
2021-06-29 10:42:27 +0800

    init project

---
+++ Mahjong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mahjong
+//
+//  Created by Tao, Wang on 2021/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
+
